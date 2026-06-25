3dde15a46c97815a97fc76b6d1e99d923404ec5d
finn <fcoop21@gmail.com>
2026-06-25 13:38:01 +0100

    new back

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import pygame
 from setings import *
+from pygame import mixer
 
 
-WIDTH, HEIGHT = 1080, 600
+WIDTH, HEIGHT = 1900, 1000
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("mugman")
 WHITE = (255,255,255)
+backround = pygame.image.load("newmenu.jpg")
 
 class Game():
     
     def __init__(self):
-        self.WIDTH = 1080
-        self.HEIGHT = 600
+        self.WIDTH = 1900
+        self.HEIGHT = 1000
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
+        self.clock = pygame.time.Clock()
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load("slowsun.mp3")
+    pygame.mixer.music.play(-1)  
     def play(self):
         pygame.init()
         run = True
         while run == True: 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
             self.screen.fill(WHITE)
             pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-            for event in pygame.event.get():
-                if event.type == pygame.quit:
-                    run = False
+            screen.blit(backround,(0,0)) 
+            pygame.display.flip()
+            self.clock.tick(60)
+        self.pygame.quit()
+        
+
+
+running = Game()
+running.play()
--- test.py
+++
@@ -1,7 +0,0 @@
-a = 0
-x = 1
-while x == 1:
-    p = ("hello world" , a)
-    print (p)
-    a += 1
-    
